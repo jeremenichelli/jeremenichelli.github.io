fcbcab1f71b02c5ab9d928625a2b696cba520b9e
Jere Menichelli <jmenichelli@gmail.com>
2014-04-21 00:11:06 -0300

    Mobile styles and scripts. Gulp task update.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	less = require('gulp-less'),
 	minifycss = require('gulp-minify-css'),
@@ -10,6 +11,16 @@ gulp.task('less', function(){
 		.pipe(less())
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 2'))
 		.pipe(minifycss())
+		.pipe(rename({
+			suffix : '.min'
+		}))
+		.pipe(gulp.dest('assets/'));
+});
+
+// js task
+gulp.task('js', function(){
+	return gulp.src('assets/**/*.js')
+		.pipe(uglify())
 		.pipe(rename({
 			suffix : '.min'
 		}))
