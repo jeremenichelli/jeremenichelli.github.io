b6f978e283d5a3bb9d488db4aa8a7d3082115a57
Jere Menichelli <jmenichelli@gmail.com>
2014-06-21 12:08:32 -0300

    Gulp tasks improve. JS clean up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,44 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	less = require('gulp-less'),
-	minifycss = require('gulp-minify-css'),
-	autoprefixer = require('gulp-autoprefixer');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer');
+    jshint = require('gulp-jshint');
 
 // less task
 gulp.task('less', function(){
-	return gulp.src('assets/**/*.less')
-		.pipe(less())
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 2'))
-		.pipe(minifycss())
-		.pipe(rename({
-			suffix : '.min'
-		}))
-		.pipe(gulp.dest('assets/'));
+    return gulp.src('src/css/*.less')
+        .pipe(less())
+        .pipe(autoprefixer('last 2 version', 'safari 6', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 2'))
+        .pipe(gulp.dest('assets/css'))
+        .pipe(minifycss())
+        .pipe(rename({
+            suffix : '.min'
+        }))
+        .pipe(gulp.dest('assets/css'));
 });
 
 // js task
 gulp.task('js', function(){
-	return gulp.src('assets/**/main.js')
-		.pipe(uglify())
-		.pipe(rename({
-			suffix : '.min'
-		}))
-		.pipe(gulp.dest('assets/'));
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix : '.min'
+        }))
+        .pipe(gulp.dest('assets/js'));
+});
+
+// jshint
+gulp.task('lint', function(){
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 // watching
 gulp.task('watch', function(){
-	gulp.watch('assets/**/*', ['less', 'js']);
+    gulp.watch('src/**/*', ['less', 'js']);
 });
