5ac9a3d6eba546fdb0c37b8beb7255dbec80d7d2
Jere Menichelli <jmenichelli@gmail.com>
2015-10-17 21:45:42 -0300

    Delete unnecessary require statement

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@
      * $: namespace for gulp plugins
      */
     var gulp = require('gulp'),
-        eslint = require('gulp-eslint'),
         $ = require('gulp-load-plugins')({
                 pattern: ['gulp-*', 'gulp.*'],
                 scope: [ 'devDependencies' ],
@@ -148,13 +147,13 @@
         return gulp.src(paths.scripts.noncritical.lint)
             // eslint() attaches the lint output to the eslint property
             // of the file object so it can be used by other modules.
-            .pipe(eslint())
+            .pipe($.eslint())
             // eslint.format() outputs the lint results to the console.
             // Alternatively use eslint.formatEach() (see Docs).
-            .pipe(eslint.format())
+            .pipe($.eslint.format())
             // To have the process exit with an error code (1) on
             // lint error, return the stream and pipe to failAfterError last.
-            .pipe(eslint.failAfterError());
+            .pipe($.eslint.failAfterError());
     });
 
     /*
