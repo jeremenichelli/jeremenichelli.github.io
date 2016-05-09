4597cadda52940e78cdd6f4da17964eb93242425
Jere Menichelli <jmenichelli@gmail.com>
2016-05-09 17:47:25 -0300

    Build process on Gulp. Font loading strategy

---
+++ gulpfile.babel.js
@@ -0,0 +1,93 @@
+import gulp from 'gulp';
+import plugins from 'gulp-load-plugins';
+import del from 'del';
+
+// namespace for gulp plugins
+const $ = plugins({
+        pattern: ['gulp-*', 'gulp.*'],
+        scope: [ 'devDependencies' ],
+        replaceString: /^gulp(-|\.)/,
+        camelize: true
+    });
+
+// paths
+const paths = {
+    less: {
+        critical: {
+            src: './src/styles/critical--*',
+            dest: './_includes/'
+        },
+        noncritical: {
+            src: './src/styles/noncritical--*',
+            dest: './assets/styles/'
+        }
+    },
+    js: {
+        critical: {
+            src: './src/scripts/critical/**/*.js',
+            dest: './_includes/'
+        }
+    },
+    favicon: {
+        src: './src/favicon/*',
+        dest: './assets/favicon/'
+    }
+};
+
+// flags
+const uncompressed = $.util.env.u;
+
+// less critical
+gulp.task('less:critical', _ => {
+    return gulp.src(paths.less.critical.src)
+        .pipe($.less())
+        .pipe(uncompressed ? $.util.noop() : $.cleanCss())
+        .pipe($.rename({
+            extname: '.html'
+        }))
+        .pipe(gulp.dest(paths.less.critical.dest));
+});
+
+// less noncritical
+gulp.task('less:noncritical', _ => {
+    return gulp.src(paths.less.noncritical.src)
+        .pipe($.less())
+        .pipe(uncompressed ? $.util.noop() : $.cleanCss())
+        .pipe($.rename(file => {
+            file.basename = file.basename.replace('noncritical--', '');
+        }))
+        .pipe(gulp.dest(paths.less.noncritical.dest));
+});
+
+// less task
+gulp.task('less', [ 'less:critical', 'less:noncritical' ]);
+
+// javascript task
+gulp.task('js:critical', _ => {
+    return gulp.src(paths.js.critical.src)
+        .pipe($.concatUtil('main.js'))
+        .pipe(uncompressed ? $.util.noop() : $.uglify())
+        .pipe($.rename({
+            basename: 'critical--js',
+            extname: '.html'
+        }))
+        .pipe(gulp.dest(paths.js.critical.dest));
+});
+
+// favicon
+gulp.task('favicon', _ => {
+    return gulp.src(paths.favicon.src)
+        .pipe(gulp.dest(paths.favicon.dest));
+});
+
+// clean
+gulp.task('clean', done => {
+    del('./assets/');
+    done();
+});
+
+// build
+gulp.task('build', [ 'favicon', 'less', 'js:critical' ]);
+
+// default task
+gulp.task('default', [ 'build' ]);
--- src/scripts/critical/main.js
+++ src/scripts/critical/main.js
@@ -1,10 +1,54 @@
-(function(PAGE) {
+(function(_win, _doc) {
     'use strict';
 
-    // Google Analytics
-    require('./_ga')();
+    var host = _win.location.hostname;
 
-    var loadCSS = require('fg-loadcss');
+    // development flag
+    var DEV = (host === 'localhost' || host === '0.0.0.0' || host === '127.0.0.1');
 
-    loadCSS('/assets/styles/' + PAGE + '.css');
-})(window.PAGE);
+    // stylesheets
+    if (DEV) {
+        _win.store.verbose();
+    }
+
+    if (_win.PAGE) {
+        // page styles
+        _win.store.css('/assets/styles/' + PAGE + '.css', {
+            storage: 'session'
+        });
+        // font face styles
+        _win.store.css('https://fonts.googleapis.com/css?family=Fira+Sans:400,400italic,700', {
+            storage: 'session',
+            crossOrigin: 'anonymous'
+        });
+    }
+
+    // fonts
+    if (_win.sessionStorage.getItem('fonts-loaded')) {
+        _doc.documentElement.classList.add('fonts-loaded');
+    } else {
+        var w400 = new FontFaceObserver('Fira Sans', { weight: 400 }),
+            w400i = new FontFaceObserver('Fira Sans', { weight: 400, style: 'italic' }),
+            w700 = new FontFaceObserver('Fira Sans', { weight: 700 });
+
+        Promise.all([ w400.load(), w400i.load(), w700.load() ])
+            .then(function() {
+                _doc.documentElement.classList.add('fonts-loaded');
+                _win.sessionStorage.setItem('fonts-loaded', true);
+            });
+    }
+
+    // analytics
+    if (!DEV) {
+        // track visit when not serving locally
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(_win,_doc,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-50290926-1', 'jeremenichelli.github.io');
+        ga('send', 'pageview');
+    } else {
+        console.log('Serving locally. Google Analytics is not tracking.');
+    }
+})(window, document);
