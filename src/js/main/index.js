8fed0313cad57ff5199ff16d7f79a92f36a9ce06
Jere Menichelli <jmenichelli@gmail.com>
2017-12-21 18:06:43 -0300

    Remove home image. Update home animation

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -6,33 +6,10 @@
     _win.FONTS_LOADED = Promise.resolve();
   }
 
-  // resolve promise when image is loaded
-  if (_win.PAGE === 'home') {
-    var IMAGE_LOADED = new Promise(function(resolve) {
-      // get home image
-      var image = _doc.getElementsByClassName('home__image')[0];
-
-      // resolve on iamge loaded
-      image.onload = function() {
-        image.onload = null;
-        resolve();
-      }
-
-      // assign src to image
-      image.src = image.dataset.src;
-    });
-  } else {
-    var IMAGE_LOADED = Promise.resolve();
-  }
-
   /*
    * add class when image and fonts are ready
    */
-  Promise
-    .all([
-      IMAGE_LOADED,
-      _win.FONTS_LOADED
-    ])
+  _win.FONTS_LOADED
     .then(function() {
       // add ready class on next available frame
       requestAnimationFrame(function() {
