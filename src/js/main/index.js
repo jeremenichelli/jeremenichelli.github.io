daf4d7bbd666228a5ad3d1b7cbead906bfc8bd1f
Jere Menichelli <jmenichelli@gmail.com>
2018-01-20 23:32:40 +0100

    Move hashed headings function to map

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -6,30 +6,24 @@
     _win.FONTS_LOADED = Promise.resolve();
   }
 
-  /*
-   * add class when image and fonts are ready
-   */
+  // Add class when image and fonts are ready
   _win.FONTS_LOADED
-    .then(function() {
-      // add ready class on next available frame
-      requestAnimationFrame(function() {
-        _doc.documentElement.classList.add('ready');
-      });
+  .then(function() {
+    // add ready class on next available frame
+    requestAnimationFrame(function() {
+      _doc.documentElement.classList.add('ready');
     });
+  });
 
-  /*
-   * Wrap subheadings with anchors in posts
-   */
+  // Wrap subheadings with anchors in posts
   if (_win.PAGE === 'post') {
-    var headings = _doc.querySelectorAll('h2, h3');
+    [].slice.call(_doc.querySelectorAll('h2, h3')).map(function(h) {
 
-    for (var i = 0, len = headings.length; i < len; i++) {
-      var h = headings[ i ];
       h.innerHTML = '<a href="#' + h.id + '">' + h.textContent + '</a>';
 
       if (DEV === true) {
         console.log('Link added to heading', '<' + h.tagName.toLowerCase() + '>', h.textContent);
       }
-    }
+    });
   }
 })(window, document);
