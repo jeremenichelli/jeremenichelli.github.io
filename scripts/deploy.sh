3bfe39bceec1f9fdb9c1da59952241bf8179fcf9
Jeremias Menichelli <jmenichelli@gmail.com>
2018-10-09 09:52:06 +0200

    Force files addition to gh-pages branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 yarn release \
 && git init \
-&& git add . \
+&& git add . --force \
 && git commit -m "Deploy site and assets to gh-pages" \
 && git push --force https://${GH_TOKEN}@github.com/jeremenichelli/jeremenichelli.github.io.git HEAD:gh-pages \
 && echo "Site and assets deployed correctly"
