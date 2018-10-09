f5424f83c1c21e59b1f04f9634254acaed19fcec
Jeremias Menichelli <jmenichelli@gmail.com>
2018-10-09 10:33:47 +0200

    Rename deploy token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 yarn release \
+&& git init \
 && echo "node_modules" > .gitignore \
-&& git config --global user.email "travis@travis-ci.org" \
-&& git config --global user.name "Travis CI" \
-&& git checkout -b gh-pages \
 && git add . \
-&& git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" \
-&& git remote add origin-pages https://${GH_TOKEN}@github.com/jeremenichelli/jeremenichelli.github.io.git \
-&& git push --quiet --set-upstream origin-pages gh-pages
+&& git commit -m "Deploy site and assets to gh-pages" \
+&& git push --force https://${GITHUB_TOKEN}@github.com/jeremenichelli/jeremenichelli.github.io.git HEAD:gh-pages \
+&& echo "Site and assets deployed correctly"
+
