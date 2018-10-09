3c36aea144af634a3338f4f1562a2927a310ec1b
Jeremias Menichelli <jmenichelli@gmail.com>
2018-10-09 10:29:32 +0200

    Fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 yarn release \
 && echo "node_modules" > .gitignore \
-&& git config --global user.email "travis@travis-ci.org"
-&& git config --global user.name "Travis CI"
-&& git checkout -b gh-pages
+&& git config --global user.email "travis@travis-ci.org" \
+&& git config --global user.name "Travis CI" \
+&& git checkout -b gh-pages \
 && git add . \
-&& git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-&& git remote add origin-pages https://${GH_TOKEN}@github.com/jeremenichelli/jeremenichelli.github.io.git
+&& git commit --message "Travis build: $TRAVIS_BUILD_NUMBER" \
+&& git remote add origin-pages https://${GH_TOKEN}@github.com/jeremenichelli/jeremenichelli.github.io.git \
 && git push --quiet --set-upstream origin-pages gh-pages
