8ea21a2154967474566734f6b31ffafd4da0947c
Jeremias Menichelli <jmenichelli@gmail.com>
2018-10-09 10:12:56 +0200

    Ignore node_modules on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 yarn release \
 && git init \
-&& git add . --force \
+&& echo "node_modules" > .gitignore \
+&& git add . \
 && git commit -m "Deploy site and assets to gh-pages" \
 && git push --force https://${GH_TOKEN}@github.com/jeremenichelli/jeremenichelli.github.io.git HEAD:gh-pages \
 && echo "Site and assets deployed correctly"
